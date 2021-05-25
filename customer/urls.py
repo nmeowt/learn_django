3e4800add5944e69bf195d7121d4aee59a184a66
NMT <nmtrangg1999@gmail.com>
2021-05-25 19:35:38 +0700

    authenticate

--- customer/urls.py
+++ customer/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import index, register_view, store_infomation
+from .views import index, login_process, login_view, logout, register_view, store_infomation
 
 urlpatterns = [
     path('', index, name="index"),
     # Hiển thị trang đăng ký
     path('register', register_view, name="register_view"),
+    # Lưu thông tin khách hàng đăng ký
+    path('store-information', store_infomation, name="store_info"),
     # Hiển thị trang đăng nhập
-    path('store-information', store_infomation, name="store-info")
+    path('login', login_view, name="login_view"),
+    # Kiểm tra đăng đăng
+    path('login-process', login_process, name="login_process"),
+    # Đăng xuất
+    path('logout', logout, name="logout"),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import redirect, render
 from .models import Category, Customer, Product
 import hashlib
+from django.urls import reverse
 
 
 def index(request):
-    # ORM của DJANGO
-    # Lấy tất cả dữ liệu của 1 bảng: Objects.all()
-    category = Category.objects.all()
-    product = Product.objects.all()
-    context = {
-        "category": category,
-        "product": product
-    }
-    # dữ liệu được đưa vào phải là dictionary {}
-    return render(request, "customer/index.html", context)
+    if 'customer' in request.session:
+        email_customer = request.session['customer']
+        # ORM của DJANGO
+        # Lấy tất cả dữ liệu của 1 bảng: Objects.all()
+        category = Category.objects.all()
+        product = Product.objects.all()
+        context = {
+            "category": category,
+            "product": product,
+            "customer": email_customer
+        }
+        # dữ liệu được đưa vào phải là dictionary {}
+        return render(request, "customer/index.html", context)
+
+    return redirect('login_view')
 
 
 def register_view(request):
+    if 'customer' in request.session:
+        return redirect('index')
+
     return render(request, "customer/register.html")
 
 
@@ -27,6 +36,41 @@ def store_infomation(request):
     customer.last_name = request.POST.get('last-name')
     customer.email = request.POST.get('email')
     password = request.POST.get('password')
-    customer.password = hashlib.md5(password.encode())
+    customer.password = hashlib.md5(password.encode()).hexdigest()
     customer.save()  # store data to database
     return redirect('index')
+
+
+def login_view(request):
+    if 'customer' in request.session:
+        return redirect('index')
+
+    context = {}
+    if request.GET.get('error'):
+        context = {'error': 'Wrong email or password'}
+    return render(request, "customer/login.html", context)
+
+
+def login_process(request):
+    if request.method == "POST":
+        # Nhận dữ liệu đã post
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        # mã hoá md5 với pass đó
+        pass_hash_md5 = hashlib.md5(password.encode()).hexdigest()
+        customer_check = Customer.objects.filter(
+            email=email, password=pass_hash_md5)
+        if customer_check:
+            request.session["customer"] = email
+            return redirect('index')
+        url = f"{reverse('login_view')}?error=true"
+        return redirect(url)
+    else:
+        return redirect('login_view')
+
+
+def logout(request):
+    if 'customer' in request.session:
+        del request.session['customer']
+        return redirect('login_view')
+    return redirect('login_view')
